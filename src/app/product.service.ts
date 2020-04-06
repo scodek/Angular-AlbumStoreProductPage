8e314ab9608f9b8b5bf07567f71a48b81f9a144a
scodek <skhalequ03@gmail.com>
2020-04-06 15:58:09 +0200

    completed module

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http,Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl : string = "../assets/album.json";
+
+  constructor(private _http:Http) { }
+
+  getAlbum(id:number) : Observable<any>{
+    return this._http.get(this._albumUrl);
+  }
+
+}
